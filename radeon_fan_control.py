d013260129ca8aa64ef9736f65949e631904f785
Aleksandr Mezin <mezin.alexander@gmail.com>
2018-08-24 07:31:43 +0600

    Parse config more careful

--- radeon_fan_control.py
+++ radeon_fan_control.py
@@ -5,17 +5,28 @@ import time
 
 
 class HwmonDevice:
-    def __init__(self, sysfs_path, pwm_min=0, pwm_max=255, temp_min=40, temp_max=85, curve_pow=2):
+    def __init__(self, sysfs_path, config):
+        def getfloat(key, default):
+            value = config.pop(key, default)
+            try:
+                return float(value)
+            except ValueError as ex:
+                print(sysfs_path, key + ":", ex)
+                return default
+
         self.sysfs_path = pathlib.Path(sysfs_path)
         self.pwm_path = self.sysfs_path / 'pwm1'
         self.temp_path = self.sysfs_path / 'temp1_input'
-        self.pwm_min = float(pwm_min)
-        self.pwm_max = float(pwm_max)
-        self.temp_min = float(temp_min)
-        self.temp_max = float(temp_max)
+        self.pwm_min = getfloat('pwm_min', 0.0)
+        self.pwm_max = getfloat('pwm_max', 255.0)
+        self.temp_min = getfloat('temp_min', 40.0)
+        self.temp_max = getfloat('temp_max', 85.0)
         self.pwm_delta = self.pwm_max - self.pwm_min
         self.temp_delta = self.temp_max - self.temp_min
-        self.curve_pow = float(curve_pow)
+        self.curve_pow = getfloat('curve_pow', 2.0)
+
+        for k in config.keys():
+            print("Unknown parameter {!r}".format(k))
 
     @property
     def temp(self):
@@ -42,7 +53,7 @@ def main(*args, **kwargs):
     config = configparser.ConfigParser()
     config.read(arg.config)
 
-    devices = [HwmonDevice(section, **config[section]) for section in config.sections()]
+    devices = [HwmonDevice(section, config[section]) for section in config.sections()]
 
     while True:
         for device in devices:
